161ac145b9d4aa4d56e1f8afb6ac0673f3c9ded1
moe <moe9999@protonmail.com>
2020-12-29 01:24:31 +0000

    Catch if user has dm's closed

--- src/main/kotlin/me/stockvip/memberships/listeners/UpgradeListener.kt
+++ src/main/kotlin/me/stockvip/memberships/listeners/UpgradeListener.kt
@@ -1,6 +1,7 @@
 package me.stockvip.memberships.listeners
 
 import com.gitlab.kordlib.core.event.message.MessageCreateEvent
+import kotlinx.coroutines.channels.ClosedSendChannelException
 import me.jakejmattson.discordkt.api.dsl.listeners
 import me.jakejmattson.discordkt.api.extensions.sendPrivateMessage
 import me.jakejmattson.discordkt.api.extensions.toSnowflake
@@ -19,17 +20,22 @@ fun upgradeListener() = listeners {
         val member = member ?: return@on
 
         if (message.content.toLowerCase().startsWith("upgrade")) {
-            member.sendPrivateMessage {
-                color = Color.decode("#FFC700")
-                title = "Please click the link below to purchase your role"
-                description = ":point_right: https://donatebot.io/checkout/708542100873936947?buyer=${member.id.value}  :point_left: \n\n" +
-                        "The role should be applied in the next 10 minutes, if you have any questions or problems please head over to <#760287662220771360> and support will be with you shortly."
+            try {
+                member.sendPrivateMessage {
+                    color = Color.decode("#FFC700")
+                    title = "Please click the link below to purchase your role"
+                    description = ":point_right: https://donatebot.io/checkout/708542100873936947?buyer=${member.id.value}  :point_left: \n\n" +
+                            "The role should be applied in the next 10 minutes, if you have any questions or problems please head over to <#760287662220771360> and support will be with you shortly."
 
-                footer {
-                    icon = "https://i.imgur.com/07U8XSA.gif"
-                    text = "StockVIP - Firm Handshakes"
+                    footer {
+                        icon = "https://i.imgur.com/07U8XSA.gif"
+                        text = "StockVIP - Firm Handshakes"
+                    }
                 }
+            } catch (ex: ClosedSendChannelException) {
+                return@on
             }
+
         }
 
 
