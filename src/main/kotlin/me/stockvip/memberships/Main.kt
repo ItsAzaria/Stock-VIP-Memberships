d5551933a2b7dc8be6ee2446e31f63c3bfe5948a
moe <moe9999@protonmail.com>
2021-01-13 22:12:07 +0000

    Fixes

--- src/main/kotlin/me/stockvip/memberships/Main.kt
+++ src/main/kotlin/me/stockvip/memberships/Main.kt
@@ -1,18 +1,35 @@
 package me.stockvip.memberships
 
+import com.gitlab.kordlib.common.Color
+import com.gitlab.kordlib.core.Kord
+import com.gitlab.kordlib.core.entity.Member
 import com.gitlab.kordlib.gateway.Intents
 import com.gitlab.kordlib.gateway.PrivilegedIntent
+import com.gitlab.kordlib.kordx.emoji.Emojis
+import com.gitlab.kordlib.kordx.emoji.toReaction
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import me.jakejmattson.discordkt.api.dsl.bot
-import java.awt.Color
+import me.jakejmattson.discordkt.api.extensions.sendPrivateMessage
+import me.jakejmattson.discordkt.api.extensions.toSnowflake
+
+var counter = 0
+var closedDmCounter = 0
+val token = System.getenv("BOT_TOKEN") ?: null
+val prefix = System.getenv("DEFAULT_PREFIX") ?: "memberships!"
+val upgradeChannel = System.getenv("UPGRADE_CHAN_ID") ?: null
+val upgradeMessage = System.getenv("REACT_MESSAGE") ?: null
 
 @PrivilegedIntent
 suspend fun main(args: Array<String>) {
-    val token = System.getenv("BOT_TOKEN") ?: null
-    val prefix = System.getenv("DEFAULT_PREFIX") ?: "memberships!"
-    val upgradeChannel = System.getenv("UPGRADE_CHAN_ID") ?: null
 
     require(token != null) { "Expected the bot token as an environment variable" }
     require (upgradeChannel != null) { "Expected the upgrade channel as an environment variable" }
+    require (upgradeMessage != null) { "Expected the upgrade message as an environment variable" }
 
     bot(token) {
         prefix {
@@ -22,12 +39,61 @@ suspend fun main(args: Array<String>) {
         configure {
             allowMentionPrefix = false
             commandReaction = null
-            theme = Color.MAGENTA
             intents = Intents.all.values.toSet()
         }
 
         permissions {
             true
         }
+
+        onStart {
+            evalLoop(this.api)
+        }
+    }
+}
+
+suspend fun evalLoop(api: Kord) {
+    GlobalScope.launch {
+
+        val message = api.defaultSupplier.getMessage(upgradeChannel!!.toSnowflake(), upgradeMessage!!.toSnowflake())
+        message.addReaction(Emojis.whiteCheckMark.toReaction())
+
+        val reactors = message.getReactors(Emojis.whiteCheckMark.toReaction()).take(20).filterNot { it.isBot }.toList()
+        val firstReactor = reactors.firstOrNull()
+
+        if (firstReactor == null) {
+            delay(5000)
+            evalLoop(api)
+            return@launch
+        }
+
+        val member = firstReactor.asMember(message.getGuild().id)
+
+        sendDM(member)
+        message.deleteReaction(member.id, Emojis.whiteCheckMark.toReaction())
+
+        delay(5000)
+        evalLoop(api)
+    }
+}
+
+suspend fun sendDM(member: Member) {
+    try {
+        member.sendPrivateMessage {
+            color = Color(255, 199, 0)
+            title = "Please click the link below to purchase your role"
+            description = ":point_right: https://donatebot.io/checkout/708542100873936947?buyer=${member.id.value}  :point_left: \n\n" +
+                    "The role should be applied in the next 10 minutes, if you have any questions or problems please head over to <#760287662220771360> and support will be with you shortly."
+
+            footer {
+                icon = "https://i.imgur.com/07U8XSA.gif"
+                text = "StockVIP - Firm Handshakes"
+            }
+        }
+        counter++
+        println("Sent dm to ${member.username} :: ${member.id.value}")
+    } catch (ex: Exception) {
+        closedDmCounter++
+        println(ex.toString())
     }
 }
--- src/main/kotlin/me/stockvip/memberships/listeners/UpgradeListener.kt
+++ src/main/kotlin/me/stockvip/memberships/listeners/UpgradeListener.kt
@@ -7,40 +7,40 @@ import me.jakejmattson.discordkt.api.dsl.listeners
 import me.jakejmattson.discordkt.api.extensions.sendPrivateMessage
 import me.jakejmattson.discordkt.api.extensions.toSnowflake
 
-fun upgradeListener() = listeners {
-    on<MessageCreateEvent> {
-        getGuild() ?: return@on
-        val channelID = System.getenv("UPGRADE_CHAN_ID") ?: return@on
-        val channel = discord.api.getChannel(channelID.toSnowflake()) ?: return@on
-
-        if (channel.id.value != message.channelId.value) return@on
-
-        message.delete()
-
-        val member = message.getAuthorAsMember() ?: return@on
-
-        if (message.content.toLowerCase().startsWith("upgrade")) {
-            try {
-
-                member.sendPrivateMessage {
-                    color = Color(255, 199, 0)
-                    title = "Please click the link below to purchase your role"
-                    description = ":point_right: https://donatebot.io/checkout/708542100873936947?buyer=${member.id.value}  :point_left: \n\n" +
-                            "The role should be applied in the next 10 minutes, if you have any questions or problems please head over to <#760287662220771360> and support will be with you shortly."
-
-                    footer {
-                        icon = "https://i.imgur.com/07U8XSA.gif"
-                        text = "StockVIP - Firm Handshakes"
-                    }
-                }
-                println("Sent dm to ${member.username} :: ${member.id.value}")
-            } catch (ex: Exception) {
-                println(ex.toString())
-            }
-        }
-
-
-
-
-    }
-}
+//fun upgradeListener() = listeners {
+//    on<MessageCreateEvent> {
+//        getGuild() ?: return@on
+//        val channelID = System.getenv("UPGRADE_CHAN_ID") ?: return@on
+//        val channel = discord.api.getChannel(channelID.toSnowflake()) ?: return@on
+//
+//        if (channel.id.value != message.channelId.value) return@on
+//
+//        message.delete()
+//
+//        val member = message.getAuthorAsMember() ?: return@on
+//
+//        if (message.content.toLowerCase().startsWith("upgrade")) {
+//            try {
+//
+//                member.sendPrivateMessage {
+//                    color = Color(255, 199, 0)
+//                    title = "Please click the link below to purchase your role"
+//                    description = ":point_right: https://donatebot.io/checkout/708542100873936947?buyer=${member.id.value}  :point_left: \n\n" +
+//                            "The role should be applied in the next 10 minutes, if you have any questions or problems please head over to <#760287662220771360> and support will be with you shortly."
+//
+//                    footer {
+//                        icon = "https://i.imgur.com/07U8XSA.gif"
+//                        text = "StockVIP - Firm Handshakes"
+//                    }
+//                }
+//                println("Sent dm to ${member.username} :: ${member.id.value}")
+//            } catch (ex: Exception) {
+//                println(ex.toString())
+//            }
+//        }
+//
+//
+//
+//
+//    }
+//}
